568c54861b43365e8994ee8e3f097eb49e272098
sshch3612 <1758974924@qq.com>
2019-05-24 11:55:51 +0800

    增加头像上传功能

--- src/axios/util.js
+++ src/axios/util.js
@@ -10,16 +10,65 @@ export async function loggedIn() {
     const res = await Request({
       url: "getUser",
       method: "get",
-      headers:{
-        token:token
+      headers: {
+        token: token
       }
     });
-    if(res.code === 200){
+    if (res.code === 200) {
       store.state.islogin = true;
       store.state.userInfo = res.message || {};
       return true;
-    }else{
+    } else {
       return false;
     }
   }
+}
+
+export function fileUpload(file,callback) {
+  const imgfile = file;
+  const Reader = new FileReader();
+  const image = new Image();
+  Reader.readAsDataURL(imgfile);
+  Reader.onload = function() {
+    console.log(this.result);
+  };
+  Reader.onerror = function() {};
+  Reader.onloadend = function() {
+    if (!this.error) {
+      image.src = this.result;
+    }
+  };
+  image.onload = function() {
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
+    // 图片原始尺寸
+    const originWidth = this.width;
+    const originHeight = this.height;
+    // 最大尺寸限制
+    const maxWidth = 200;
+    const maxHeight = 200;
+    // // 目标尺寸
+    let targetWidth = originWidth;
+    let targetHeight = originHeight;
+    if (originWidth > maxWidth || originHeight > maxHeight) {
+      if (originWidth / originHeight > maxWidth / maxHeight) {
+        // 更宽，按照宽度限定尺寸
+        targetWidth = maxWidth;
+        targetHeight = Math.round(originHeight * (maxWidth / originWidth));
+      } else {
+        targetHeight = maxHeight;
+        targetWidth = Math.round(originWidth * (maxHeight / originHeight));
+      }
+    }
+    // canvas对图片进行压缩
+    canvas.width = targetWidth;
+    canvas.height = targetHeight;
+    // 清除画布
+    context.clearRect(0, 0, targetWidth, targetHeight);
+    // 图片压缩
+    context.drawImage(image, 0, 0, targetWidth, targetHeight);
+    const compressResult = canvas.toDataURL("image/png", 0.2); //base64 格式
+    console.info(compressResult, 777777);
+    callback(compressResult)
+  };
 }
--- src/pages/Mine/Index.vue
+++ src/pages/Mine/Index.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="mine">
     <mu-list textline="two-line">
-      <mu-list-item
-        avatar
-        button
-        v-model="user"
-        :ripple="false"
-        v-for="(value,index) in user"
-        :key="index"
-      >
+      <mu-list-item avatar button :ripple="false" :key="index">
         <mu-list-item-content>
-          <mu-list-item-title>{{value['key']}}</mu-list-item-title>
+          <mu-list-item-title>头像</mu-list-item-title>
         </mu-list-item-content>
-        <mu-list-item-action>{{value['value']}}</mu-list-item-action>
+        <mu-list-item-action>
+          <mu-avatar :size="42">
+            <img :src="userInfo.avatar">
+          </mu-avatar>
+        </mu-list-item-action>
+        <mu-list-item-action>
+          <mu-button icon>
+            <mu-icon value="keyboard_arrow_right"></mu-icon>
+          </mu-button>
+        </mu-list-item-action>
+        <input class="mine-avatar" type="file" name="file" accept="image/*" @change="updateAvatar">
+      </mu-list-item>
+      <mu-list-item avatar button :ripple="false" :key="index">
+        <mu-list-item-content>
+          <mu-list-item-title>昵称</mu-list-item-title>
+        </mu-list-item-content>
+        <mu-list-item-action>{{userInfo.username}}</mu-list-item-action>
         <mu-list-item-action>
           <mu-button icon>
             <mu-icon value="keyboard_arrow_right"></mu-icon>
@@ -21,29 +30,29 @@
       </mu-list-item>
     </mu-list>
     <mu-flex justify-content="center" align-items="center" style="margin-top:100px">
-      <mu-button full-width large color="#f44336"  @click="signout">账号退出</mu-button>
+      <mu-button full-width large color="#f44336" @click="signout">账号退出</mu-button>
     </mu-flex>
   </div>
 </template>
 <script>
 import Request from "@/axios/request";
+import { fileUpload } from "@/axios/util";
 export default {
   data: function() {
     return {
-      user: [
-        {
-          key: "头像",
-          value: this.$store.state.userInfo.avatar
-        },
-        {
-          key: "昵称",
-          value: this.$store.state.userInfo.username
-        }
-      ]
+      // userInfo: this.$store.getters.userInfo
     };
   },
+  computed: {
+    userInfo: {
+      get() {
+        return this.$store.getters.userInfo;
+      }
+    }
+  },
+
   methods: {
-    async signout() {
+    signout: async function() {
       try {
         const token = localStorage.getItem("chat_token");
         const res = await Request({
@@ -55,15 +64,50 @@ export default {
         });
         if (res.code === 200) {
           // this.$toast.message('账号退出')
-          this.$store.dispatch("changeUserinfo",{});
-          this.$router.replace({path:'/login'})
+          this.$store.dispatch("changeUserinfo", {});
+          this.$router.replace({ path: "/login" });
         }
       } catch (error) {}
+    },
+    updateAvatar: async function(event) {
+      const imgfile = event.target.files[0];
+      const _this = this;
+      fileUpload(imgfile, async function(imgbase) {
+        try {
+          const token = localStorage.getItem("chat_token");
+          const res = await Request({
+            url: "updateAvatar",
+            method: "post",
+            headers: {
+              token: token
+            },
+            data: {
+              avatar: imgbase
+            }
+          });
+          if (res.code === 200) {
+            // this.$toast.message('账号退出')
+            await _this.$store.dispatch("changeUserinfo", res.message);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      });
     }
   }
 };
 </script>
-
 <style lang="less">
+.mine {
+  &-avatar {
+    position: absolute;
+    left: 0;
+    top: 0;
+    height: 100%;
+    width: 100%;
+    opacity: 0;
+    z-index: 2;
+  }
+}
 </style>
 
