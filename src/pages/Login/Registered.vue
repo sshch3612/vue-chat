83b1ded77e8579a1bddf881b6dde2c919e729854
sshch3612 <1758974924@qq.com>
2019-05-26 11:52:56 +0800

    Refactoring the login page

--- src/pages/Login/Registered.vue
+++ src/pages/Login/Registered.vue
@@ -1,22 +1,30 @@
 <template>
   <mu-container class="registed">
-    <mu-form
-      ref="form"
-      :model="validateForm"
-      class="mu-demo-form"
-      label-position="left"
-      label-width="85"
-    >
-      <mu-form-item label="用户名" prop="username" :rules="usernameRules">
-        <mu-text-field type="text" v-model="validateForm.username" prop="username"></mu-text-field>
+    <mu-form ref="form" :model="validateForm" class="mu-demo-form">
+      <mu-form-item prop="username" :rules="usernameRules">
+        <mu-text-field
+          type="text"
+          placeholder="用户名"
+          v-model="validateForm.username"
+          prop="username"
+        ></mu-text-field>
       </mu-form-item>
-      <mu-form-item label="密码" prop="password" :rules="passwordRules">
-        <mu-text-field type="password" v-model="validateForm.password" prop="password"></mu-text-field>
+      <mu-form-item prop="password" :rules="passwordRules">
+        <mu-text-field
+          :type="visibility ? 'text' : 'password'"
+          placeholder="密码"
+          v-model="validateForm.password"
+          prop="password"
+          :action-icon="visibility ? 'visibility_off' : 'visibility'"
+          :action-click="() => (visibility = !visibility)"
+        ></mu-text-field>
       </mu-form-item>
       <mu-form-item>
-        <mu-button color="primary" @click="submit">登陆</mu-button>
-        <mu-button @click="registed">注册</mu-button>
+        <mu-button full-width round color="primary" @click="submit">登陆</mu-button>
+        <!-- <mu-button color="primary" @click="submit">登陆</mu-button> -->
+        <mu-button full-width round @click="registed">我要注册</mu-button>
       </mu-form-item>
+      <span>注:登陆、注册公用此页面</span>
     </mu-form>
   </mu-container>
 </template>
@@ -25,6 +33,7 @@ import Request from "@/axios/request";
 export default {
   data() {
     return {
+      visibility: false,
       usernameRules: [
         { validate: val => !!val, message: "必须填写用户名" },
         { validate: val => val.length >= 3, message: "用户名长度大于3" }
@@ -54,9 +63,6 @@ export default {
           });
           if (res.token && res.code === 200) {
             localStorage.setItem("chat_token", res.token);
-            this.$store.dispatch("changeUserinfo", {
-              ...this.$data.validateForm
-            });
             //登陆之后进行注册
             this.$socket.emit("registered", {
               userId: this.$data.validateForm.username,
@@ -79,7 +85,7 @@ export default {
             method: "post",
             data: { ..._this.$data.validateForm }
           });
-          _this.$toast.message(res.message);
+          _this.$alert(res.message, null,{okLabel:'我知道了'});
         }
       });
     }
@@ -88,14 +94,17 @@ export default {
 </script>
 <style>
 .registed {
-  position: fixed;
+  /* position: fixed;
   left: 0;
   top: 0;
   width: 100%;
-  height: 100%;
+  height: 100%; */
+  display: flex;
+  justify-content: center;
 }
 .mu-demo-form {
   width: 100%;
+  padding-top:60px;
   max-width: 460px;
   position: absolute;
   top: 10%;
